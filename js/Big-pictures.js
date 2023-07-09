1020c21318fdf9c6a4022620eca225841b562d5c
Natalia Zabava <natysenok78@gmail.com>
2023-07-09 15:19:59 +0300

    Добавляет обработчики событий и функции открытия и закрытия  модального окна

--- js/Big-pictures.js
+++ js/Big-pictures.js
@@ -1,46 +1,33 @@
-const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const container = document.querySelector('.pictures');
+import { isEscapeKey } from './util.js';
+import { renderThumbnails } from './thumbnails.js';
 
-const createThumbnail = ({ url, description, likes, comments}) => {
-  const thumbnail = thumbnailTemplate.cloneNode(true);
-  thumbnail.querySelector('.picture__img').src = url;
-  thumbnail.querySelector('.picture__img').alt = description;
-  thumbnail.querySelector('.picture__comments').textContent = comments.length;
-  thumbnail.querySelector('.picture__likes').textContent = likes;
-  return thumbnail;
-};
+const modalWindow = document.querySelector('.big_picture');
+const modalWindowOpenElement = document.querySelector('.pictures');
+const modalWindowCloseElement = document.querySelector('.big-picture__cancel');
 
-const renderThumbnails = (pictures) => {
-  const fragment = document.createDocumentFragment();
-  pictures.forEach((picture) => {
-    const thumbnail = createThumbnail(picture);
-    fragment.append(thumbnail);
-  });
-  container.append(fragment);
-};
-
-export { renderThumbnails };
-
-const modalWindow = document.querySelector('body');
-const bigPicture = document.querySelector('.big_picture');
-const bigPictureOpen = document.querySelector('.picture');
-const bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
-
-bigPictureOpen.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  bigPicture.classList.remove('hidden');
-  modalWindow.classList.add('modal-open');
-});
-
-bigPictureClose.addEventListener('click', () => {
-  bigPicture.classList.add('hidden');
-  modalWindow.classList.remove('modal-open');
-});
-
-document.addEventListener('keydown', (evt) => {
-  if (evt.key === 'Escape') {
+const onModalWindowEscape = (evt) => {
+  if (isEscapeKey(evt)) {
     evt.preventDefault();
-    bigPicture.classList.add('hidden');
-    modalWindow.classList.remove('modal-open');
+    closeModalWindow();
   }
+}
+
+function openModalWindow () {
+  modalWindow.classList.remove('hidden');
+  document.addEventListener('keydown', onModalWindowEscape);
+  bodyElement.classList.add('modal-open');
+};
+
+function closeModalWindow () {
+  modalWindow.classList.add('hidden');
+  document.addEventListener('keydown', onModalWindowEscape);
+  bodyElement.classList.remove('modal-open');
+};
+
+modalWindowOpenElement.addEventListener('click', () => {
+  openModalWindow();
+});
+
+modalWindowCloseElement.addEventListener('click', () => {
+  closeModalWindow();
 });
