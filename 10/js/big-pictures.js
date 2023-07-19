1a1b8cba9d668b60a6f7e73f7d9cac536e9ffa64
Keks <keks@htmlacademy.ru>
2023-07-19 07:38:28 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/big-pictures.js
+++ 10/js/big-pictures.js
@@ -1,8 +1,31 @@
 import { isEscapeKey } from './util.js';
 
-const bigPicture = document.querySelector('.big_picture');
+const bigPicture = document.querySelector('.big-picture');
 const bodyElement = document.querySelector('body');
 const modalWindowCloseElement = document.querySelector('.big-picture__cancel');
+const commentElement = document.querySelector('.social__comment');
+const commentListElement = document.querySelector('social__comments');
+const commentsLoaderElement = document.querySelector('social__comments-loader');
+const commentCountElement = document.querySelector('social__comment-count');
+
+const createComment = ({ avatar, name, message }) => {
+  const comment = commentElement.cloneNode(true);
+  const commentImg = comment.querySelector('.social__picture');
+  commentImg.src = avatar;
+  commentImg.alt = name;
+  comment.querySelector('.social__text').textContent = message;
+  return comment;
+};
+
+const renderComments = (comments) => {
+  commentListElement.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+  comments.forEach((item) => {
+    const comment = createComment(item);
+    fragment.append(comment);
+  });
+  commentListElement.append(fragment);
+};
 
 const closeModalWindow = () => {
   bigPicture.classList.add('hidden');
@@ -29,7 +52,10 @@ const openBigPicture = (dataPicture) => {
   bigPicture.classList.remove('hidden');
   document.addEventListener('keydown', onModalWindowEscape);
   bodyElement.classList.add('modal-open');
+  commentCountElement.classList.add('hidden');
+  commentsLoaderElement.classList.add('hidden');
   renderPictureInformation(dataPicture);
+  renderComments(dataPicture.comments);
 };
 
 modalWindowCloseElement.addEventListener('click', () => {
