16dae5e55a14b89a90e78964be1474a20ab5873a
Keks <keks@htmlacademy.ru>
2023-07-23 15:17:08 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/upload-picture.js
+++ 10/js/upload-picture.js
@@ -1,39 +1,44 @@
 import { isEscapeKey } from './util.js';
+import { resetScale, addButtonScaleHandler } from './scale.js';
+import { addSliderEffectHandler, hideSlider, resetEffect} from './slider-effects.js';
 
-const form = document.querySelector('.img-upload__form');
-const overlay = document.querySelector('.img-upload__overlay');
+const MAX_HASHTAGS_COUNT = 5;
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1-19}$/i;
 const body = document.querySelector('body');
-const pictureField = document.querySelector('.img-upload__input');
-const hashtagsField = document.querySelector('.text__hashtags');
-const commentsField = document.querySelector('.text__description');
-const cancelButton = document.querySelector('.img-upload__cancel');
-
-
-const openEditorPicture = () => {
-  overlay.classList.remove('hidden');
-  body.classList.add('modal-open');
-  document.addEventListener('keydown', onModalWindowEscape)
-};
+const form = document.querySelector('.img-upload__form');
+const overlay = form.querySelector('.img-upload__overlay');
+const pictureField = form.querySelector('.img-upload__input');
+const hashtagsField = form.querySelector('.text__hashtags');
+const commentsField = form.querySelector('.text__description');
+const cancelButton = form.querySelector('.img-upload__cancel');
+const submitButton = form.querySelector('.img-upload__submit');
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
 });
 
-const MAX_HASHTAGS_COUNT = 5;
-const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1-19}$/i;
-const hashtags = hashtagsField.value.split(' ');
-const validateHashtag = () => hashtags.every((hashtag) => VALID_SYMBOLS.test(hashtag));
-const validateHashtagCount = () => hashtags.length <= MAX_HASHTAGS_COUNT;
+const openEditorPicture = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onModalWindowEscape);
+  addButtonScaleHandler();
+  addSliderEffectHandler();
+  hideSlider();
+};
 
-const validateUniqueHashtagName = () => {
-  const UpperCaseHashtag = hashtags.map((hashtag) => hashtag.toUpperCase());
+const editHashtag = (hashtagString) => hashtagString.trim().split(' ').filter((hashtag) => Boolean(hashtag.length));
+
+const validateHashtag = (value) => editHashtag(value).every((hashtag) => (hashtag.match(VALID_SYMBOLS)));
+const validateHashtagCount = (value) => editHashtag(value).length <= MAX_HASHTAGS_COUNT;
+const validateUniqueHashtagName = (value) => {
+  const UpperCaseHashtag = editHashtag(value).map((hashtag) => hashtag.toUpperCase());
   return UpperCaseHashtag.length === new Set(UpperCaseHashtag).size;
 };
 
-pristine.addValidator(hashtagsField, validateHashtagCount, 'не больше 5');
-pristine.addValidator(hashtagsField, validateHashtag, 'Невалидный хэш-тэг');
-pristine.addValidator(hashtagsField, validateUniqueHashtagName, 'Повторяющийся хэш-тэг');
+pristine.addValidator(hashtagsField, validateHashtagCount, 'не больше 5', 3, true);
+pristine.addValidator(hashtagsField, validateHashtag, 'Невалидный хэш-тэг', 2, true);
+pristine.addValidator(hashtagsField, validateUniqueHashtagName, 'Повторяющийся хэш-тэг', 1, true);
 
 
 const closeEditorPicture = () => {
@@ -42,6 +47,8 @@ const closeEditorPicture = () => {
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onModalWindowEscape);
   pristine.reset();
+  resetScale();
+  resetEffect();
 };
 
 const isFieldFocus = () => document.activeElement === hashtagsField || document.activeElement === commentsField;
@@ -61,4 +68,26 @@ const uploadPicture = () => {
   pictureField.addEventListener('change', openEditorPicture);
 };
 
-export { uploadPicture };
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Идет загрузка';
+};
+
+const unBlockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+const setOnFormSubmit = (callback) => {
+  form.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    if (isValid) {
+      blockSubmitButton();
+      await callback(new FormData(form));
+      unBlockSubmitButton();
+    }
+  });
+};
+
+export { setOnFormSubmit, uploadPicture, closeEditorPicture};
